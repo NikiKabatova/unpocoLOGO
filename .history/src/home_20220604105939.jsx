7d902456e42cd2774ec4bf36c562395ce2dd0dc4
Sylva21 <sylva@Fuskas-MacBook-Pro.local>
2022-06-04 11:01:05 +0200

    Vytvoření Home a About

---
+++ .history/src/home_20220604105939.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './img/logo.svg';
+
+const Home = () => {
+  return (
+    <main>
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
