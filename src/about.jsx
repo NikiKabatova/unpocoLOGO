f53229f3b4714b9f8185ace1b0206279c04522fd
Sylva21 <sylva@Fuskas-MacBook-Pro.local>
2022-06-04 12:27:06 +0200

    Uprava textu

--- src/about.jsx
+++ src/about.jsx
@@ -8,34 +8,27 @@ const About = () => {
           <h1>Info o hře</h1>
           <p>
             Ahoj rodičové, rád bych Vám pověděl pár slov o logopedii a proč je
-            dobré s ní začínat už v útlém věku. Jistě všichni ze zkušenosti
-            víme, že rodič je tím nejlepším a nejkvalitnějším vzorem pro své
-            děti. Nejen, že jste spolu každý den, ale děti rády napodobují to co
-            vidí a slyší. A to je největší hybnou silou ve vývoji komunikačních
-            a jazykových schopností dítěte.
+            dobré s ní začínat už v útlém věku. Všichni ze zkušenosti víme, že
+            rodič je tím nejlepším vzorem pro své děti. Trávíte spolu každý den
+            a děti rády napodobují to co vidí a slyší. To je největší hybnou
+            silou ve vývoji komunikačních a jazykových schopností dítěte.
           </p>
           <p>
             Preventivní logopedickou péči v podobě rozvoje je vhodné aplikovat
-            už od útlého věku dítěte, již batolata a následně děti v MŠ
-            (pedagogové by měli být v oblasti logopedické prevence vzdělaní).
-          </p>
-          <p>
-            Velmi důležitá je komunikace v rodině, její podoba a celkové návyky
-            v rodině. Jakou roli má mluvené slovo, jak moc se využívá – to vše
-            ovlivňuje dítě. Hra je nejvhodnější forma pro učení dětí
-            předškolního věku. Součástí této hry budete i vy, aby jste dětem
-            pomohli správně rozvíjet řečové schopnosti.
+            už od útlého věku dítěte. Velmi důležitá je komunikace v rodině.
+            Jakou roli má mluvené slovo, jak moc se využívá – to vše ovlivňuje
+            dítě. Hra je nejvhodnější forma pro učení dětí předškolního věku.
+            Součástí této hry budete i vy, aby jste dětem pomohli správně
+            rozvíjet řečové schopnosti.
           </p>
           <p>
             Podle průzkumu, který jsem provedl já sám je zastaralé tvrdit, že na
-            logopedii je čas, víte, slýchávám to ze všech stran – od pediatrů i
-            rodičů. Je to výmluvná a omluvná hláška, která schovává nejistotu a
-            nechuť začít dělat něco právě teď. Logoped může poradit mamince nebo
-            tatínkovi dvouletého dítěte aktivity, které realizují v pohodě doma
-            a logopedii, v 5 letech se dítě pak krásně může vyhnout a nemusí na
-            ni docházet, protože rodiče správně zacílili aktivity v batolecím
-            věku. Budu moc rád, když podpoříte své milované ratolesti tím, že si
-            s nimi hru zahrajete.
+            logopedii je čas, víte, slýchávám to ze všech stran. Pouze to
+            schovává nejistotu a nechuť začít dělat něco právě teď. Logoped může
+            poradit rodičům dvouletého dítěte aktivity, které realizují v pohodě
+            doma, v 5 letech už na ní nemusí docházet, protože rodiče správně
+            zacílili aktivity v batolecím věku. Budu moc rád, když podpoříte své
+            milované ratolesti tím, že si s nimi hru zahrajete.
           </p>
         </div>
       </main>
