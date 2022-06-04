68029796197dec70c191e9edb45f0f001c1f4ddd
Nikola Kabátová <niki.kabatova@gmail.com>
2022-06-04 16:11:22 +0200

    add new logo + title + import font family

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../img/logo.svg';
-import '../style.css';
+import './style.css';
 const Home = () => {
   return (
     <main>
       <div className="container">
         <img className="logo" src={logo} />
+        <p className="logo-title">un poco LOGO</p>
         <button className="button1">Start</button>
         <button className="button2">O hře</button>
       </div>
---
+++ src/Map/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () => {
+  return (
+    <main>
+      <img></img>
+    </main>
+  );
+};
+
+export default Map;
