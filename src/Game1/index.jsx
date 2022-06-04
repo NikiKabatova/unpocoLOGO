a3aecebb40429614109e8c7163b4ea1101187353
Nikola Kabátová <niki.kabatova@gmail.com>
2022-06-04 19:18:20 +0200

    logo test

--- src/Game1/index.jsx
+++ src/Game1/index.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import heart from './heart.png';
+const Game1 = () => {
+  return (
+    <div>
+      <div className="panel">
+        <button>home</button>
+        <img className="lives" src={heart} />
+      </div>
+    </div>
+  );
+};
+
+export default Game1;
