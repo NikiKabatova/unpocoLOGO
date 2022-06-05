eadcd9a96b096b3a2555b6f93070ca1f3b49c301
Nikola Kabátová <niki.kabatova@gmail.com>
2022-06-05 10:03:56 +0200

    positions of levels in Map component

--- src/Map/index.jsx
+++ src/Map/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import map from './map.png';
-import level1 from './level1.png';
+import map from './img/map.png';
+import level1 from './img/level1.png';
+import level2 from './img/level2.png';
+import level3 from './img/level3.png';
 import './style.css';
 
 const Map = () => {
@@ -8,6 +10,8 @@ const Map = () => {
     <div className="map">
       <img className="map__image" src={map} />
       <img className="level1" src={level1} />
+      <img className="level2" src={level2} />
+      <img className="level3" src={level3} />
     </div>
   );
 };
