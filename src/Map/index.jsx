b16e90b0c2143175ba8b0a01192135a238ebd0dd
Nikola Kabátová <niki.kabatova@gmail.com>
2022-06-05 11:01:21 +0200

    condition for changing levels pictures

--- src/Map/index.jsx
+++ src/Map/index.jsx
@@ -3,11 +3,17 @@ import map from './img/map.png';
 import level1 from './img/level1.png';
 import level2 from './img/level2.png';
 import level3 from './img/level3.png';
+import cave from './img/cave.png';
+import waterfall from './img/waterfall.png';
+import vulcano from './img/vulcano.png';
+
 import './style.css';
 
 const Map = ({ unlockedLevels }) => {
   console.log(unlockedLevels);
-  level1 = unlockedLevels[1] ? 'levelGray' : 'levelColor';
+  level1 = unlockedLevels[1] ? level1 : cave;
+  level2 = unlockedLevels[1] ? level2 : waterfall;
+  level3 = unlockedLevels[1] ? level3 : vulcano;
   return (
     <div className="map">
       <img className="map__image" src={map} />
