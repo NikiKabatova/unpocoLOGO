22c2f6633d7eaf47e0e336bc68dd3964ddcd6da1
Nikola Kabátová <niki.kabatova@gmail.com>
2022-06-11 23:27:08 +0200

    Style Victory popup

--- src/Rules/index.jsx
+++ src/Rules/index.jsx
@@ -10,20 +10,20 @@ const Rules = () => {
       <nav className="navigation">
         <Button image="home" target="/" className="home__button" />
       </nav>
-
-      <p>
+      <div className="game-rules__content">
+        <p>
+          Ahoj, já jsem Brokolík, tvým úkolem bude mi pomoci zachránit princeznu
+          Karotku ze spárů zlouna Čilíka. Postupně budeme procházet jednotlivá
+          stanoviště a plnit úkoly, s úkoly ti pomohou rodiče. Pokud se ti
+          podaří splnit úkol, posouváš se na další stanoviště, ve hře máš tři
+          životy, díky kterým můžeš úkol opakovat. K osvobození princezny je
+          musíš splnit všechny. Tak vzhůru do boje.
+        </p>
         <img className="brokolik" src={brokolik} />
-        Ahoj, já jsem Brokolík, tvým úkolem bude mi pomoci zachránit princeznu
-        Karotku ze spárů zlouna Čilíka. Postupně budeme procházet jednotlivá
-        stanoviště a plnit úkoly, s úkoly ti pomohou rodiče. Pokud se ti podaří
-        splnit úkol, posouváš se na další stanoviště, ve hře máš tři životy,
-        díky kterým můžeš úkol opakovat. K osvobození princezny je musíš splnit
-        všechny. Tak vzhůru do boje.
-      </p>
-
-      <button className="button1">
-        <Link to="/map">Start</Link>
-      </button>
+        <button className="button1">
+          <Link to="/map">Start</Link>
+        </button>
+      </div>
     </main>
   );
 };
--- src/VictoryPopup/index.jsx
+++ src/VictoryPopup/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './style.css';
 import { Link, useNavigate } from 'react-router-dom';
+import homeIcon from './home.png';
+import reloadIcon from './refresh.png';
+import checkIcon from './check.png';
+
+import stars from './stars.png';
 
 const VictoryPopup = () => {
   const navigate = useNavigate();
@@ -9,16 +14,34 @@ const VictoryPopup = () => {
   };
   return (
     <div className="rules-info">
-      <div className="bubble">
-        <h1>Huráá</h1>
+      <div className="popup">
+        <img className="stars" src={stars} alt="Obrázek tří hvězd" />
+        <h1 className="popup__title">Skvělá práce</h1>
         <p>Teď už jenom zbývá aby ti rodiče potvrdili úspěšně zdolaný level!</p>
+
         <Link to="/">
-          <button>Home</button>
+          <button className="home__button">
+            <img className="button__icon" src={homeIcon} alt="Ikona domečku" />
+          </button>
         </Link>
-        <button onClick={refreshPage}>Reload</button>
+
         <Link to="/map">
-          <button>OK</button>
+          <button className="check__button">
+            <img
+              className="button__icon"
+              src={checkIcon}
+              alt="Ikona s potvrzovací fajfkou"
+            />
+          </button>
         </Link>
+
+        <button className="reload__button" onClick={refreshPage}>
+          <img
+            className="button__icon"
+            src={reloadIcon}
+            alt="Ikona znovunačtení"
+          />
+        </button>
       </div>
     </div>
   );
